f1a87c4b6c4b942fa87c2f7f3b5783c2c80d5151
progmatic-99 <shubham.upreti@gmail.com>
2024-02-29 12:28:27 +0530

    chore: config changes for deployment

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 """Environment Config"""
 
+import os
 from dotenv import dotenv_values
 
-
-config = dotenv_values(".env")
-bot_token = config["ACCESS_TOKEN"]
-usernames = config["USERNAMES"]
+if os.environ.get("DEPLOYMENT") == "PROD":
+    bot_token = os.environ.get("ACCESS_TOKEN")
+else:
+    config = dotenv_values(".env")
+    bot_token = config["ACCESS_TOKEN"]
+    usernames = config["USERNAMES"]
