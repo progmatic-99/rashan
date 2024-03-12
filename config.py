1540304d2548bd51cecec30fc4d13326b67559c8
progmatic-99 <shubham.upreti@gmail.com>
2024-03-12 22:41:27 +0530

    change db path

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ config = dotenv_values(".env")
 bot_token = config["ACCESS_TOKEN"]
 deployment = config["DEPLOYMENT"] or None
 
-db_path = "/var/db/rashan.sqlite3" if deployment else "./rashan.sqlite"
+db_path = "/home/ec2-user/rashan.sqlite3" if deployment else "./rashan.sqlite"
 
