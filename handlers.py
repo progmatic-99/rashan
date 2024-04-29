3d86e6997bd30798b7231a6ae54f037c4a8f96bc
progmatic-99 <shubham.upreti@gmail.com>
2024-04-30 00:35:29 +0530

    dont log data

--- handlers.py
+++ handlers.py
@@ -184,7 +184,6 @@ async def items_usage(update: Update, ctx: ContextTypes.DEFAULT_TYPE) -> None:
     db = DB()
     try:
         result = db.get_monthly_usage(curr_month, curr_year)
-        logger.info(result)
     except sqlite3.Error as e:
         logger.error(e)
         await update.message.reply_text(e)
@@ -247,7 +246,6 @@ async def last_purchased_item(update: Update, ctx: ContextTypes.DEFAULT_TYPE) ->
         db = DB()
         try:
             result = db.search_item(item)
-            logger.info(result)
         except sqlite3.Error as e:
             logger.error(e)
             await update.message.reply_text(e)
